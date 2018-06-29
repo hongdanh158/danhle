99aaaad2275b8f5e7a013b948dea2f5ad8015ca9
Lê Trương Hồng Danh <lthdanh@gmail.com>
2018-06-29 16:25:48 +0700

    Initial commit

---
+++ footer.php
@@ -0,0 +1,14 @@
+    <footer class="footer">
+      
+    </footer>
+  </div>
+  <div class="scrollToTop"><i class="fas fa-arrow-up"></i></div>
+
+  <!-- slick -->
+  <link rel="stylesheet" href="js/slick/slick.css">
+  <script src="js/slick/slick.min.js""></script>
+  <!-- End slick -->
+
+  <script defer src="js/script.js?v=1"></script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Danh Lê</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=vietnamese" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
+    <link href="css/style.css?v=1" rel="stylesheet">
+    <link href="css/responsive.css?v=1" rel="stylesheet">
+    <script src="js/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="body" canvas="container">
+	    <header class="header">
+	      
+	    </header>
